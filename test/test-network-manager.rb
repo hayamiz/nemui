1140500a54b6a6413747aee88091192dd42536d3
Yuto Hayamizu <y.hayamizu@gmail.com>
2009-09-14 16:47:58 +0900

    add test_properties of network-manager

--- test/test-network-manager.rb
+++ test/test-network-manager.rb
@@ -20,6 +20,19 @@ class NetworkManagerTest < Test::Unit::TestCase
            NEMUI::NetworkManager::NM_STATE_DISCONNECTED == @nm.state)
   end
 
+  def test_properties
+    assert_boolean(@nm[:wireless_enabled])
+    assert_boolean(@nm[:wireless_hardware_enabled])
+    assert_included_in([NEMUI::NetworkManager::NM_STATE_UNKNOWN,
+                        NEMUI::NetworkManager::NM_STATE_ASLEEP,
+                        NEMUI::NetworkManager::NM_STATE_CONNECTING,
+                        NEMUI::NetworkManager::NM_STATE_CONNECTED,
+                        NEMUI::NetworkManager::NM_STATE_DISCONNECTED],
+                       @nm[:state])
+    assert_instance_array_of(NEMUI::NetworkManagerSettings::Connection,
+                             @nm[:active_connections])
+  end
+
   def teardown
     begin
       @nm.wake
